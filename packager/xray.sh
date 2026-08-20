bdc45865da48a164a82714b8dc4cae28465bd516
Markson Pigeonzilla Plus <markson@archlinux.local>
2026-08-20 21:13:02 +0800

    fix: some fucking architects has more than one alias

--- packager/xray.sh
+++ packager/xray.sh
@@ -3,8 +3,5 @@ REAL_VERSION=$(cat xray_version.txt)
 for debian_arch in amd64 arm64 i386 riscv64; do
     cat nfpm/xray.yaml | sed "s/REAL_VERSION/$REAL_VERSION/g" | sed "s/REAL_ARCH/${debian_arch}/g" | tee /tmp/xray_${REAL_VERSION}_${debian_arch}_debian.yaml
     nfpm package -p deb --config /tmp/xray_${REAL_VERSION}_${debian_arch}_debian.yaml --target ./archive/xray_${REAL_VERSION}_${debian_arch}.deb
-done
-for rpm_arch in x86_64 aarch64 i386 riscv64; do
-    cat nfpm/xray.yaml | sed "s/REAL_VERSION/$REAL_VERSION/g" | sed "s/REAL_ARCH/${rpm_arch}/g" | tee /tmp/xray_${REAL_VERSION}_${rpm_arch}_rpm.yaml
-    nfpm package -p rpm --config /tmp/xray_${REAL_VERSION}_${rpm_arch}_rpm.yaml --target ./archive/xray_${REAL_VERSION}_${rpm_arch}.rpm
+    nfpm package -p rpm --config /tmp/xray_${REAL_VERSION}_${debian_arch}_rpm.yaml --target ./archive/xray_${REAL_VERSION}_${debian_arch}.rpm
 done
