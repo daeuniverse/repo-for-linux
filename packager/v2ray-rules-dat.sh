f216a6aad4eea0f86281ba6f03ba7a04710c3c08
Markson Pigeonzilla Plus <markson@archlinux.local>
2026-08-20 21:23:58 +0800

    fix: add missing v2ray-rules-dat.yaml to shell script

--- packager/v2ray-rules-dat.sh
+++ packager/v2ray-rules-dat.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 REAL_VERSION=$(cat v2ray-rules-dat_version.txt)
+cat nfpm/v2ray-rules-dat.yaml | sed "s/REAL_VERSION/$REAL_VERSION/g" | tee /tmp/v2ray-rules-dat.yaml
 nfpm package --packager deb --config /tmp/v2ray-rules-dat.yaml --target ./archive/v2ray-rules-dat_${REAL_VERSION}_all.deb
 nfpm package --packager rpm --config /tmp/v2ray-rules-dat.yaml --target ./archive/v2ray-rules-dat_${REAL_VERSION}_all.rpm
