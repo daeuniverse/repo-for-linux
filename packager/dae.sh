33ace5a416292f8432ebc29bb8fdeb69c93a8fd2
Markson Pigeonzilla Plus <markson@archlinux.local>
2026-08-20 21:00:11 +0800

    fix: missing archive folder

--- packager/dae.sh
+++ packager/dae.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+[ -d archive ] || mkdir -p archive
 REAL_VERSION=$(cat dae_version.txt)
 for debian_arch in amd64 arm64 i386 riscv64; do
     cat nfpm/dae.yaml | sed "s/REAL_VERSION/$REAL_VERSION/g" | sed "s/REAL_ARCH/${debian_arch}/g" | tee /tmp/dae_${REAL_VERSION}_${debian_arch}_debian.yaml
